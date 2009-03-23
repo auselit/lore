b3c1bc33caf2b1d3cbe88eb75b57a50f906c5e4a
Anna Gerber <anna.m.gerber@gmail.com>
2009-03-23 07:25:35 +0000

    Use AustLit servers by default for annotations and compound objects

--- defaults/preferences/oaiorebuilder.js
+++ defaults/preferences/oaiorebuilder.js
@@ -1,5 +1,5 @@
 pref("extensions.lore.relonturl", "chrome://oaiorebuilder/content/ontologies/austlitoaiore.owl");
 pref("extensions.lore.dccreator","Anonymous");
-pref("extensions.lore.rdfrepos", "http://localhost:8081/openrdf-sesame/repositories/LORE");
+pref("extensions.lore.rdfrepos", "http://austlit.edu.au/openrdf-sesame/repositories/lore");
 pref("extensions.lore.rdfrepostype", "sesame");
-pref("extensions.lore.annoserver", "http://localhost:8081/danno/annotea");
+pref("extensions.lore.annoserver", "http://austlit.edu.au/danno/annotea");
