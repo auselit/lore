7105b2a4660ce84448bd1c234e70cbacc3f2f05e
Anna Gerber <anna.m.gerber@gmail.com>
2009-04-30 01:50:55 +0000

    Better logging support using Firebug trace console

--- oaiorebuilder/defaults/preferences/oaiorebuilder.js
+++ oaiorebuilder/defaults/preferences/oaiorebuilder.js
@@ -3,3 +3,7 @@ pref("extensions.lore.dccreator","Anonymous");
 pref("extensions.lore.rdfrepos", "http://austlit.edu.au/openrdf-sesame/repositories/lore");
 pref("extensions.lore.rdfrepostype", "sesame");
 pref("extensions.lore.annoserver", "http://austlit.edu.au/danno/annotea");
+pref("extensions.lore.DBG_LORE_UI", false);
+pref("extensions.lore.DBG_LORE_ANNOTATIONS", false);
+pref("extensions.lore.DBG_LORE_COMPOUND_OBJECTS", false);
+pref("extensions.lore.DBG_LORE_TEXT_MINING", false);
