c2a1bc3f4b8ae87724c653b2a1af60570324550b
Anna Gerber <anna.m.gerber@gmail.com>
2009-06-16 00:30:13 +0000

    renamed to loreprefs.js and added added some preferences for ui customisation

--- defaults/preferences/loreprefs.js
+++ defaults/preferences/loreprefs.js
@@ -1,4 +1,4 @@
-pref("extensions.lore.relonturl", "chrome://oaiorebuilder/content/ontologies/austlitoaiore.owl");
+pref("extensions.lore.relonturl", "chrome://lore/content/ontologies/austlitoaiore.owl");
 pref("extensions.lore.dccreator","Anonymous");
 pref("extensions.lore.rdfrepos", "http://austlit.edu.au/openrdf-sesame/repositories/lore");
 pref("extensions.lore.rdfrepostype", "sesame");
@@ -7,3 +7,6 @@ pref("extensions.lore.DBG_LORE_UI", false);
 pref("extensions.lore.DBG_LORE_ANNOTATIONS", false);
 pref("extensions.lore.DBG_LORE_COMPOUND_OBJECTS", false);
 pref("extensions.lore.DBG_LORE_TEXT_MINING", false);
+pref("extensions.lore.disable_annotations", false);
+pref("extensions.lore.disable_compoundobjects", false);
+pref("extensions.lore.disable_textmining", false);
