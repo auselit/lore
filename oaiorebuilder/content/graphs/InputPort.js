3ee0c2c02aa7e38d2c71fbd1963dd1ee63362caa
Anna Gerber <anna.m.gerber@gmail.com>
2009-09-03 05:58:05 +0000

    #89 make node connection target larger

--- oaiorebuilder/content/graphs/InputPort.js
+++ oaiorebuilder/content/graphs/InputPort.js
@@ -3,6 +3,7 @@
  */
 lore.ore.graph.Port = function(uirep) {
     draw2d.Port.call(this,uirep);
+    this.setCoronaWidth(35);
 };
 lore.ore.graph.Port.prototype = new draw2d.Port;
 lore.ore.graph.Port.prototype.type = "lore.ore.graph.Port";
