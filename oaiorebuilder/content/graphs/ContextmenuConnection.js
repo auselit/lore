402bf8cc1577b914f2593210449d87b8c3f174df
Anna Gerber <anna.m.gerber@gmail.com>
2009-09-03 04:04:33 +0000

    Updated connections so that there are ports on node top and bottom, and arrows are anchored at node center instead of on the specific ports, in response to #89 and #31. No longer need input and output ports, so changed to use just a single port type. Note: now this is annoying when there are cyclic connections as they are displayed over the top of each other.

--- oaiorebuilder/content/graphs/ContextmenuConnection.js
+++ oaiorebuilder/content/graphs/ContextmenuConnection.js
@@ -2,32 +2,35 @@
  * Connection figure that provides a context menu to set the connection type
  * The types are sourced from ontrelationships, which is populated from an ontology (see uifunctions.js)
  */
-lore.ore.graph.ContextmenuConnection=function()
-{draw2d.Connection.call(this);
-var grey = new draw2d.Color(174,174,174);
-var darkgrey = new draw2d.Color(51,51,51);
-var theArrow = new draw2d.ArrowConnectionDecorator();
-theArrow.setColor(grey);
-this.setTargetDecorator(theArrow);
-this.setRouter(new draw2d.BezierConnectionRouter());
-this.label = new draw2d.Label();
-this.label.setColor(darkgrey);
-this.addFigure(this.label, new draw2d.ManhattanMidpointLocator(this));
-this.setRelationshipType("http://purl.org/vocab/frbr/core#","partOf");
-this.sourcePort=null;this.targetPort=null;
-this.lineSegments=new Array();
-this.setColor(grey);
-this.setLineWidth(1);
+lore.ore.graph.ContextmenuConnection=function(){
+	draw2d.Connection.call(this);
+	var grey = new draw2d.Color(174,174,174);
+	var darkgrey = new draw2d.Color(51,51,51);
+	var theArrow = new draw2d.ArrowConnectionDecorator();
+	theArrow.setColor(grey);
+	this.setTargetDecorator(theArrow);
+	this.setRouter(new draw2d.BezierConnectionRouter());
+	//this.setRouter(new draw2d.ManhattanConnectionRouter());
+	this.label = new draw2d.Label();
+	this.label.setColor(darkgrey);
+	this.addFigure(this.label, new draw2d.ManhattanMidpointLocator(this));
+    this.setRelationshipType("http://purl.org/dc/elements/1.1/","relation");
+	this.sourcePort=null;
+    this.targetPort=null;
+	this.lineSegments=new Array();
+	this.setColor(grey);
+	this.setLineWidth(1);
+	this.setSourceAnchor(new draw2d.ChopboxConnectionAnchor());
+	this.setTargetAnchor(new draw2d.ChopboxConnectionAnchor());
 };
 lore.ore.graph.ContextmenuConnection.prototype=new draw2d.Connection();
-lore.ore.graph.ContextmenuConnection.prototype.setRelationshipType=function(enamespace, etype)
-{this.edgetype=etype;
-this.edgens=enamespace;
-lore.ui.nodegrid.setSource({"Relationship":etype, "Schema": enamespace});
-this.label.setText(etype);
+lore.ore.graph.ContextmenuConnection.prototype.setRelationshipType=function(enamespace, etype){
+	this.edgetype=etype;
+	this.edgens=enamespace;
+	lore.ui.nodegrid.setSource({"Relationship":etype, "Schema": enamespace});
+	this.label.setText(etype);
 };
-lore.ore.graph.ContextmenuConnection.prototype.getContextMenu=function()
-{
+lore.ore.graph.ContextmenuConnection.prototype.getContextMenu=function() {
 	var menu=new draw2d.Menu();
 	var oThis = this;
 	// sort the menu entries
