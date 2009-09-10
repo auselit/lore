256ee58d986d99802298d114cdda379c6cf72e79
Andrew Hyland <amhyland82@gmail.com>
2009-09-10 01:19:22 +0000

    Default preferences now having the annotation server url set to maenad-auselit.cloud...etc

--- defaults/preferences/loreprefs.js
+++ defaults/preferences/loreprefs.js
@@ -2,7 +2,7 @@ pref("extensions.lore.relonturl", "chrome://lore/content/ontologies/austlitoaior
 pref("extensions.lore.dccreator","Anonymous");
 pref("extensions.lore.rdfrepos", "http://austlit.edu.au/openrdf-sesame/repositories/lore");
 pref("extensions.lore.rdfrepostype", "sesame");
-pref("extensions.lore.annoserver", "http://austlit.edu.au/danno/annotea");
+pref("extensions.lore.annoserver", "http://maenad-auselit.cloud.itee.uq.edu.au/danno/annotea");
 pref("extensions.lore.DBG_LORE_UI", false);
 pref("extensions.lore.DBG_LORE_ANNOTATIONS", false);
 pref("extensions.lore.DBG_LORE_COMPOUND_OBJECTS", false);
