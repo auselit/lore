a7b73daea0254a2847fb343d08269eeff0cf34b6
Anna Gerber <anna.m.gerber@gmail.com>
2009-09-17 00:34:21 +0000

    rename InputPort to Port

--- content/compound_objects/graphical_editor/Port.js
+++ content/compound_objects/graphical_editor/Port.js
@@ -5,13 +5,12 @@ lore.ore.graph.Port = function(uirep) {
     draw2d.Port.call(this,uirep);
     this.setCoronaWidth(35);
 };
-lore.ore.graph.Port.prototype = new draw2d.Port;
+lore.ore.graph.Port.prototype = new draw2d.Port();
 lore.ore.graph.Port.prototype.type = "lore.ore.graph.Port";
 lore.ore.graph.Port.prototype.onDrop = function(port) {
 	if (this.parentNode.id != port.parentNode.id) {
-		var _5322 = new draw2d.CommandConnect(this.parentNode.workflow, this,
-				port);
-		_5322.setConnection(new lore.ore.graph.ContextmenuConnection());
-		this.parentNode.workflow.getCommandStack().execute(_5322);
+		var commConn = new draw2d.CommandConnect(this.parentNode.workflow, this, port);
+		commConn.setConnection(new lore.ore.graph.ContextmenuConnection());
+		this.parentNode.workflow.getCommandStack().execute(commConn);
 	}
 };
