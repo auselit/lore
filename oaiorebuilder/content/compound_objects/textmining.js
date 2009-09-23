5aa2f675dfed4ab4d793e76d749930267bd482ee
Anna Gerber <anna.m.gerber@gmail.com>
2009-09-23 06:05:05 +0000

    moved text mining to ore (plus namespace renaming)

--- oaiorebuilder/content/compound_objects/textmining.js
+++ oaiorebuilder/content/compound_objects/textmining.js
@@ -17,38 +17,23 @@
  * You should have received a copy of the GNU General Public License along with
  * LORE. If not, see <http://www.gnu.org/licenses/>.
  */
-/**
- * Text mining aspects of LORE
- * @namespace
- * @name lore.textm
- */
+ 
+ /*
+  * @include  "/oaiorebuilder/content/compound_objects/RdfStore.js"
+  * @include "/oaiorebuilder/content/compound_objects/RdfGrid.js"
+  */
+  
 /**
  * Handler function for successful return of Ajax request to Calais web service
  * 
  * @param {Object} resp
  */
-lore.textm.handleOpenCalaisMetadata = function(resp) {
-    // get the contents of the string element
-    // using string replace because the result using the dom was being truncated
-    var res = resp.responseText.replace(
-            '<?xml version="1.0" encoding="utf-8"?>', '').replace(
-            '<string xmlns="http://clearforest.com/">', '').replace(
-            '</string>', '');
-    // load into an object
-    var jsonObj;
-    lore.ui.textminingtab.body.update(res + lore.textm.POWEREDBY_CALAIS);
-    var nativeJSON = Components.classes["@mozilla.org/dom/json;1"]
-                 .createInstance(Components.interfaces.nsIJSON);
-
-    var jsonObj = nativeJSON.decode(res);
-
-}
-lore.textm.processRDFa = function() {
+lore.ore.textm.processRDFa = function(tmtab) {
     try{
     // process RDFa in current page
     var doc = lore.global.util.getContentWindow(window).document;
     var contentElem = jQuery('body',doc);
-    lore.ui.textminingtab.body.update("Found the following from RDFa:<br>");
+    tmtab.body.update("Found the following from RDFa:<br>");
     var myrdf = contentElem.rdfa();
     var triples = myrdf.databank.triples();
     lore.debug.tm("triples object", triples);
@@ -58,65 +43,80 @@ lore.textm.processRDFa = function() {
         var triplestr = lore.global.util.escapeHTML(triple.toString());
         // add a border around elements with rdfa with a hover to disply the triple
         if (triple.source.style){
-	        triple.source.style.border="0.5px solid #eeeeee";
-	        jQuery(triple.source).simpletip({
-	            content: '&nbsp;<img src="chrome://lore/skin/icons/rdf.png">&nbsp;' +  triplestr,
-	            showEffect: 'custom',
-	            fixed:true,
+            triple.source.style.border="0.5px solid #eeeeee";
+            jQuery(triple.source).simpletip({
+                content: '&nbsp;<img src="chrome://lore/skin/icons/rdf.png">&nbsp;' +  triplestr,
+                showEffect: 'custom',
+                fixed:true,
                 position:["-20","0"],
-	            showCustom: function(){
-	              try{
-	                //lore.debug.ore("showcustom",this);
-	                this.context.style.padding="2px";
-	                this.context.style.position= 'absolute';
-	                this.context.style.opacity = "1";
-	                this.context.style.backgroundColor="#fcfcfc";
-	                this.context.style.fontSize = "9pt";
-	                this.context.style.fontWeight = "normal";
-	                this.context.style.color="#51666b";  
-	                this.context.style.border= '1.5px solid orange';
-	                this.context.style.zIndex="3";  
-	                jQuery(this).animate({
-	                    width: 'auto',
-	                    display: 'block'
-	                },400);
+                showCustom: function(){
+                  try{
+                    //lore.debug.ore("showcustom",this);
+                    this.context.style.padding="2px";
+                    this.context.style.position= 'absolute';
+                    this.context.style.opacity = "1";
+                    this.context.style.backgroundColor="#fcfcfc";
+                    this.context.style.fontSize = "9pt";
+                    this.context.style.fontWeight = "normal";
+                    this.context.style.color="#51666b";  
+                    this.context.style.border= '1.5px solid orange';
+                    this.context.style.zIndex="3";  
+                    jQuery(this).animate({
+                        width: 'auto',
+                        display: 'block'
+                    },400);
                     
-	              } catch (ex){
-	                lore.debug.ore("exception in hover",ex);
-	              }
-	            }
-	        });
+                  } catch (ex){
+                    lore.debug.ore("exception in hover",ex);
+                  }
+                }
+            });
         }    
     }
     myrdf.prefix('rdf',lore.constants.NAMESPACES["rdf"]).where('?athing rdf:type ?atype').each(function(){
        
        var resultStr = this.athing.value + " is a " + this.atype.value + "<br>";
-       lore.ui.textminingtab.body.insertHtml('beforeEnd',resultStr);
+       tmtab.body.insertHtml('beforeEnd',resultStr);
        myrdf.about(this.athing).each(function(){
-        lore.debug.ore("properties of thing "+ this.property.value.toString(),this);
+        //lore.debug.ore("properties of thing "+ this.property.value.toString(),this);
         
        });
        
     });
+    
+    /*var myrdfstore = new Ext.ux.RdfStore(myrdf.databank); 
+
+    var rdfgrid = new Ext.ux.RdfGrid({
+       store: myrdfstore
+    });
+
+    var win = new Ext.Window({
+       items: rdfgrid,
+       width: 500,
+       height:350,
+       plain: true
+    });
+    win.show(this);
+    lore.debug.ore("grid created",rdfgrid);*/
     var ser = new XMLSerializer();
     var rdfxml = jQuery.rdf.dump(triples,{'format': 'application/rdf+xml', 'namespaces': {'austlit': 'http://austlit.edu.au/owl/austlit.owl#'}});
-    lore.ui.textminingtab.body.insertHtml('beforeEnd',"The RDF/XML is:<pre style='font-size:smaller;color:#51666b;'>"
+    tmtab.body.insertHtml('beforeEnd',"The RDF/XML is:<pre style='font-size:smaller;color:#51666b;'>"
         + Ext.util.Format.htmlEncode(XML(ser.serializeToString(rdfxml)).toXMLString()) + "</pre>");
-    
     } catch (ex){
         lore.debug.tm("exception processing rdfa",ex);
     }
 }
+
 /**
  * Triggered when the user selects the Calais button from the toolbar: sends a
  * request to retrieve metadata using Calais web service
  */
-lore.textm.requestTextMiningMetadata = function() {
-    if (!lore.ui.textminingtab) {
-        return;
-    }
+lore.ore.textm.requestTextMiningMetadata = function(tmtab) {
+    lore.debug.tm("requested text mining");
     
-    lore.textm.processRDFa();
+    // TODO: do this async
+    lore.ore.textm.processRDFa(tmtab);
+    
     // TODO: call semantic text mining services such as Open Calais
     /*var ocParams = '<c:params xmlns:c="http://s.opencalais.com/1/pred/">'
             + '<c:processingDirectives c:contentType="text/txt" c:outputFormat="application/json"></c:processingDirectives>'
@@ -126,7 +126,7 @@ lore.textm.requestTextMiningMetadata = function() {
     // set contentStr to current main window contents
     // TODO: do this better in terms of preserving whitespace and ignoring
     // script and hidden elems
-    var contentStr = lore.global.util.escapeHTML(lore.global.util.normalize(window.top.getBrowser().selectedBrowser.contentWindow.document.body.textContent));
+
     // alert(contentStr);
     // truncate - web service can only handle 100,000 chars
     if (contentStr.length > 99999) {
@@ -138,7 +138,7 @@ lore.textm.requestTextMiningMetadata = function() {
         success : lore.textm.handleOpenCalaisMetadata,
         failure : function(resp) {
             lore.debug.tm("Unable to obtain OpenCalais metadata", resp);
-            lore.ui.loreWarning("Unable to obtain OpenCalais metadata");
+            lore.ore.ui.loreWarning("Unable to obtain OpenCalais metadata");
         },
         params : {
             licenseID : lore.textm.OPENCALAIS_KEY,
@@ -147,5 +147,21 @@ lore.textm.requestTextMiningMetadata = function() {
         }
     });*/
    
-    //lore.ui.textminingtab.body.update(myresult);
+    //lore.ore.ui.textminingtab.body.update(myresult);
 }
+/*lore.ore.textm.handleOpenCalaisMetadata = function(resp) {
+    // get the contents of the string element
+    // using string replace because the result using the dom was being truncated
+    var res = resp.responseText.replace(
+            '<?xml version="1.0" encoding="utf-8"?>', '').replace(
+            '<string xmlns="http://clearforest.com/">', '').replace(
+            '</string>', '');
+    // load into an object
+    var jsonObj;
+    lore.ore.ui.textminingtab.body.update(res + lore.ore.textm.POWEREDBY_CALAIS);
+    var nativeJSON = Components.classes["@mozilla.org/dom/json;1"]
+                 .createInstance(Components.interfaces.nsIJSON);
+
+    var jsonObj = nativeJSON.decode(res);
+
+}*/
