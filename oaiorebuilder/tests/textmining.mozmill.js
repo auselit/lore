b959c88dd842fbf5c771e2d931e2c7392d125048
Anna Gerber <anna.m.gerber@gmail.com>
2009-09-28 05:20:26 +0000

    Tests for rdfa

---
+++ oaiorebuilder/tests/textmining.mozmill.js
@@ -0,0 +1,23 @@
+
+var jumlib = {};
+Components.utils.import("resource://mozmill/modules/jum.js", jumlib);
+
+var setupModule = function(module) {
+  controller = mozmill.getBrowserController();
+}
+
+var testLoadRdfaPage = function (){
+  controller.open("chrome://loretest/content/agent_rdfa.html");
+  controller.waitForPageLoad(controller.tabs.activeTab);
+}
+
+var testOpenLore = function (){
+   if (!controller.window.loreoverlay.compoundObjectsVisible()){
+     controller.click(new elementslib.ID(controller.window.document, "loreStatusIcon"));
+   }
+   jumlib.assertTrue(controller.window.loreoverlay.compoundObjectsVisible(),'lore compound objects should be open');
+};
+
+var testProcessRDFa = function(){
+  
+};
