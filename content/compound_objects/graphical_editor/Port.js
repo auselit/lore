f3547a73881726ceac0d2f63e97591a16d546005
Anna Gerber <anna.m.gerber@gmail.com>
2010-02-09 05:54:24 +0000

    jsdoc

--- content/compound_objects/graphical_editor/Port.js
+++ content/compound_objects/graphical_editor/Port.js
@@ -1,5 +1,25 @@
-/* Port that accepts connections from ContextmenuConnections 
+/*
+ * Copyright (C) 2008 - 2009 School of Information Technology and Electrical
+ * Engineering, University of Queensland (www.itee.uq.edu.au).
  * 
+ * This file is part of LORE. LORE was developed as part of the Aus-e-Lit
+ * project.
+ * 
+ * LORE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * LORE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * LORE. If not, see <http://www.gnu.org/licenses/>.
+ */
+/** 
+ * Port that accepts connections from ContextmenuConnections 
+ * @class lore.ore.graph.Port
  */
 lore.ore.graph.Port = function(uirep) {
     draw2d.Port.call(this,uirep);
@@ -7,6 +27,10 @@ lore.ore.graph.Port = function(uirep) {
 };
 lore.ore.graph.Port.prototype = new draw2d.Port;
 lore.ore.graph.Port.prototype.type = "lore.ore.graph.Port";
+/**
+ * Create a connection between nodes if a port from another node is dropped on this port
+ * @param {} port
+ */
 lore.ore.graph.Port.prototype.onDrop = function(port) {
 	if (this.parentNode.id != port.parentNode.id) {
 		var commConn = new draw2d.CommandConnect(this.parentNode.workflow, this, port);
