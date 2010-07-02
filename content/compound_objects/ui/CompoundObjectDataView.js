adde413847488cb15ef4cea9591cea4a5420edf6
Anna Gerber <anna.m.gerber@gmail.com>
2010-07-02 05:35:52 +0000

    Improved template for CO Dataview and added context menu options: the nodes in the dataview should appear and behave much the same as the nodes from the old trees

--- content/compound_objects/ui/CompoundObjectDataView.js
+++ content/compound_objects/ui/CompoundObjectDataView.js
@@ -1,22 +1,148 @@
 lore.ore.ui.CompoundObjectDataView = Ext.extend(Ext.DataView, {
     initComponent : function(){
         Ext.apply(this, { 
-            plugins: new Ext.DataView.DragSelector({dragSafe:true}),                  
+            plugins: new Ext.DataView.DragSelector({dragSafe:true}),
             tpl :  new Ext.XTemplate(               
-                "<tpl for=\".\">",
-                "<div class='coListing' onclick='lore.ore.readRDF(\"{uri}\")' style='list-style-image: url(chrome://lore/skin/icons/oaioreicon-sm.png);cursor:pointer;font-size:smaller;'>{title}",
-                    "<div style='color:#51666b;font-size:90%'>{creator}",
-                        "<tpl if='typeof modified != \"undefined\" && modified != null'>, Last modified {modified}</tpl>",
-                        "<tpl if='typeof accessed != \"undefined\" &&accessed != null'>, Last accessed {accessed}</tpl>",
-                    "</div>",
-                "</div>",
-                "</tpl>"
+                '<tpl for=".">',
+                '<div class="coListing" onclick="lore.ore.readRDF(\'{uri}\')">',
+                    '<table><tr valign="top"><td>{[this.genNumber(values.uri)]}</td><td>',
+                    '<div style="float:left;padding:2px;">',
+                    '<tpl if="lore.ore.reposAdapter && uri.match(lore.ore.reposAdapter.idPrefix)"><img src="chrome://lore/skin/icons/oaioreicon-sm.png"></tpl>',
+                    '<tpl if="lore.ore.reposAdapter && !uri.match(lore.ore.reposAdapter.idPrefix)"><img src="chrome://lore/skin/icons/oaioreicon-grey.png"></tpl>',
+                    '</div>',
+                    '<div>{title}</div>',
+                    '<div style="padding-left:20px;color:#51666b;font-size:90%">{creator}',
+                        '<tpl if="typeof modified != \'undefined\' && modified != null">, last modified {[fm.date(values.modified,\'j M Y, g:ia\')]}</tpl>',
+                        '<tpl if="typeof accessed != \'undefined\' && accessed != null">Last accessed {[fm.date(values.accessed,\'j M Y, g:ia\')]}</tpl>',
+                    '</div>',
+                    '</td></tr></table>',
+                '</div>',
+                '</tpl>',
+                {
+                    dv: this,
+                    genNumber: function(uri){
+                        var idx = this.dv.store.find('uri',uri);
+                        return this.dv.store.lastOptions.params.start + idx + 1;
+                    }
+                }
             ),
+            loadingText: "Loading compound objects...",
             singleSelect: true,
             autoScroll: true,
             itemSelector : "div.coListing"
         });
         lore.ore.ui.CompoundObjectDataView.superclass.initComponent.call(this,arguments); 
+        this.on('contextmenu',this.onContextMenu,this);
+        
+        // When data is loaded, refresh view to reset numbering
+        this.store.on("load", 
+            function(){
+                this.refresh();
+                // auto raise browse view when results are loaded
+                if (this.id == 'cobview'){
+                    if (!this.parentPanel) {
+                        this.parentPanel = this.findParentByType('panel');
+                    }
+                    if (this.parentPanel){
+                        lore.debug.ore("activating " + this.id,this.parentPanel);
+                        Ext.getCmp("propertytabs").activate(this.parentPanel.id);
+                    }
+                }
+            },
+            this
+        );
+    },
+    onContextMenu: function (e, node, obj){ 
+        var elem = e.getTarget(this.itemSelector, 10);
+        this.sel = this.getRecord(elem);
+        try{
+            if (!this.contextMenu){
+                var cm = new Ext.menu.Menu({
+                    id : this.id + "-context-menu",
+                    showSeparator: false
+                });
+                this.contextMenu = cm;
+                cm.remoteMsg = new Ext.form.Label({
+                        text : "Compound object is not from default repository",
+                        cls: 'underlined'
+                 });
+                cm.add(cm.remoteMsg);
+                cm.localLoad = new Ext.menu.Item({
+                    text: "Edit compound object",
+                       iconCls: "edit-icon",
+                       scope: this,
+                       handler: function(obj,evt) {
+                            lore.debug.ore("edit " + this.sel.data.uri);
+                            lore.ore.readRDF(this.sel.data.uri);
+                        }
+                    });
+                 cm.remoteLoad = new Ext.menu.Item({
+                    text: "View compound object in editor (read-only)",
+                       iconCls: "edit-icon",
+                       scope: this,
+                       handler: function(obj,evt) {
+                            lore.debug.ore("view " + this.sel.data.uri);
+                            lore.ore.readRDF(this.sel.data.uri);
+                        }
+                    });
+                 
+                 cm.add(cm.localLoad);
+                 cm.add(cm.remoteLoad);
+                 cm.localDelete = new Ext.menu.Item({
+                    text : "Delete compound object",
+                    iconCls: "delete-icon",
+                    scope: this,
+                    handler : function(obj,evt) {
+                        lore.debug.ore("delete handler " + this.sel.data.uri,[this,obj, evt]);
+                        lore.ore.deleteFromRepository(this.sel.data.uri, this.sel.data.title);
+                    }
+                 });
+                 
+                 cm.add(cm.localDelete);
+                 
+                 cm.add({
+                    text : "Add as nested compound object",
+                    iconCls: "add-icon",
+                    scope: this,
+                    handler : function(obj, evt) {
+                        lore.ore.ui.graphicalEditor.addFigure({url:this.sel.data.uri,
+                            props:{
+                            "rdf:type_0": lore.constants.RESOURCE_MAP,
+                            "dc:title_0": this.sel.data.title}});
+                 }});
+                 if (this.id == 'cohview'){
+                    cm.add({
+                       text: "Do not show in history",
+                       iconCls: "no-icon",
+                       scope: this,
+                       handler: function(obj,evt) {
+                            if (lore.ore.historyManager){
+                                lore.ore.historyManager.deleteFromHistory(this.sel.data.uri);
+                            }
+                        }
+                    });
+                 }
+                 
+                 
+                 cm.on('beforeshow',function(menu){
+                    if(lore.ore.reposAdapter && this.sel.data.uri.match(lore.ore.reposAdapter.idPrefix)){
+                        menu.localLoad.show();
+                        menu.localDelete.show();
+                        menu.remoteMsg.hide();
+                        menu.remoteLoad.hide();
+                    } else {
+                        menu.remoteLoad.show();
+                        menu.remoteMsg.show();
+                        menu.localDelete.hide();
+                        menu.localLoad.hide();
+                    }
+                 },this);
+                        
+            }
+            this.contextMenu.showAt(e.xy);
+        } catch (ex){
+            lore.debug.ore("onContextMenu",ex);
+        }
     }
 });
 Ext.reg('codataview', lore.ore.ui.CompoundObjectDataView);
