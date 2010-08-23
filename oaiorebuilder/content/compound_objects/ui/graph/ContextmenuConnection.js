854663a9b53306e63f0b41a6b17a4c3d4541bbca
Anna Gerber <anna.m.gerber@gmail.com>
2010-08-23 04:02:23 +0000

    changed style to use Ext.extend

--- oaiorebuilder/content/compound_objects/ui/graph/ContextmenuConnection.js
+++ oaiorebuilder/content/compound_objects/ui/graph/ContextmenuConnection.js
@@ -23,133 +23,137 @@
  * @class lore.ore.ui.graph.ContextmenuConnection
  * @extends draw2d.Connection
  */
-lore.ore.ui.graph.ContextmenuConnection=function() {
-	draw2d.Connection.call(this);
-	this.lineColor = new draw2d.Color(174,174,174); // light grey
-	this.labelColor = new draw2d.Color(51,51,51); // dark grey
-    this.label = new draw2d.Label();
-    this.label.setColor(this.labelColor);
-    this.addFigure(this.label, new lore.ore.ui.graph.LabelLocator(this));
-	this.setRouter(new draw2d.BezierConnectionRouter());
-    this.setRelationshipType("http://purl.org/dc/elements/1.1/","relation",false);
-	this.sourcePort=null;
-    this.targetPort=null;
-	this.lineSegments=[];
-	this.setColor(this.lineColor);
-	this.setLineWidth(1);
-    var tgtArrow = new lore.ore.ui.graph.ArrowConnectionDecorator();
-    tgtArrow.setColor(this.lineColor);
-    this.setTargetDecorator(tgtArrow); 
-	this.setSourceAnchor(new draw2d.ChopboxConnectionAnchor());
-	this.setTargetAnchor(new draw2d.ChopboxConnectionAnchor());
-    this.label.html.className="ctxtConnLabel";
-};
-
-lore.ore.ui.graph.ContextmenuConnection.prototype=new draw2d.Connection;
-lore.ore.ui.graph.ContextmenuConnection.prototype.getId = function(){
-    return this.id;
-}
-/** Set relationship with undo support */
-lore.ore.ui.graph.ContextmenuConnection.prototype.setRelationship = function(enamespace, etype, symmetric) {
-    this.workflow.getCommandStack().execute(new lore.ore.ui.graph.CommandSetRelationship(this, enamespace, etype, symmetric));
-};
-/**
- * Update the type of relationship that the connection represents
- * @param {} enamespace
- * @param {} etype
- * @param {} symmetric
- */
-lore.ore.ui.graph.ContextmenuConnection.prototype.setRelationshipType=function(enamespace, etype, symmetric) {
-	this.edgetype=etype;
-	this.edgens=enamespace;
-    this.symmetric = symmetric;
-	this.label.setText(etype);
-    try {
-	    if (symmetric) {
-	        var theArrow = new draw2d.ArrowConnectionDecorator();
-	        theArrow.setColor(this.lineColor);
-	        this.setSourceDecorator(theArrow);
-	    } else {
-	        this.setSourceDecorator(null);
-	    }
-    } catch(ex) {
-        lore.debug.ore("Exception setting connection rel type",ex);
+lore.ore.ui.graph.ContextmenuConnection = Ext.extend(draw2d.Connection, {
+    constructor: function() {
+    	draw2d.Connection.call(this);
+    	this.lineColor = new draw2d.Color(174,174,174); // light grey
+    	this.labelColor = new draw2d.Color(51,51,51); // dark grey
+        this.label = new draw2d.Label();
+        this.label.setColor(this.labelColor);
+        this.addFigure(this.label, new lore.ore.ui.graph.LabelLocator(this));
+    	this.setRouter(new draw2d.BezierConnectionRouter());
+        this.setRelationshipType("http://purl.org/dc/elements/1.1/","relation",false);
+    	this.sourcePort=null;
+        this.targetPort=null;
+    	this.lineSegments=[];
+    	this.setColor(this.lineColor);
+    	this.setLineWidth(1);
+        var tgtArrow = new lore.ore.ui.graph.ArrowConnectionDecorator();
+        tgtArrow.setColor(this.lineColor);
+        this.setTargetDecorator(tgtArrow); 
+    	this.setSourceAnchor(new draw2d.ChopboxConnectionAnchor());
+    	this.setTargetAnchor(new draw2d.ChopboxConnectionAnchor());
+        this.label.html.className="ctxtConnLabel";
+    },
+    /**
+     * Return the id
+     * @return {}
+     */
+    getId : function(){
+        return this.id;
+    },
+    /** Set relationship with undo support */
+    setRelationship : function(enamespace, etype, symmetric) {
+        this.workflow.getCommandStack().execute(new lore.ore.ui.graph.CommandSetRelationship(this, enamespace, etype, symmetric));
+    },
+    /**
+     * Update the type of relationship that the connection represents
+     * @param {} enamespace
+     * @param {} etype
+     * @param {} symmetric
+     */
+    setRelationshipType : function(enamespace, etype, symmetric) {
+    	this.edgetype=etype;
+    	this.edgens=enamespace;
+        this.symmetric = symmetric;
+    	this.label.setText(etype);
+        try {
+    	    if (symmetric) {
+    	        var theArrow = new draw2d.ArrowConnectionDecorator();
+    	        theArrow.setColor(this.lineColor);
+    	        this.setSourceDecorator(theArrow);
+    	    } else {
+    	        this.setSourceDecorator(null);
+    	    }
+        } catch(ex) {
+            lore.debug.ore("Exception setting connection rel type",ex);
+        }
+    },
+    createHTMLElement : function() {
+        var item = draw2d.Connection.prototype.createHTMLElement.call(this);
+        item.className = "ctxtConn";
+        return item;
+    },
+    /**
+     * Construct the context menu for selecting the connection type
+     * @return {}
+     */
+    getContextMenu : function() {
+        try {
+    	    // use the cached menu if the relationship ontology has not changed
+    	    if (lore.ore.ui.graph.ContextmenuConnection.contextMenu && (
+    	            lore.ore.ui.graph.ContextmenuConnection.loadedOntology == lore.ore.onturl)) {
+    	        return lore.ore.ui.graph.ContextmenuConnection.contextMenu;
+    	    } else {
+    	        lore.debug.ore("generating context menu for connection",this);
+    		    lore.ore.ui.graph.ContextmenuConnection.contextMenu=new draw2d.Menu();
+    			// sort the menu entries alphabetically
+                var om = lore.ore.ontologyManager;
+    			var keys = [];
+    		 	for (var rel in om.ontrelationships) {
+    				keys.push(rel);
+    		 	}
+    		 	keys.sort();
+                
+    		 	for (var i =0; i< keys.length; i++) {
+    		 		rel = keys[i];
+    				var relnamespace=om.ontrelationships[rel]; 
+    	            var symmquery = om.ontology.prefix('rdf',lore.constants.NAMESPACES["rdf"])
+    	                .where('<' + relnamespace + rel +'> rdf:type <' + lore.constants.OWL_SPROP + '>');
+    		        var symm = symmquery.length > 0;
+    				var functionstr = "lore.ore.ui.graphicalEditor.getSelectedFigure().setRelationship(\"" + relnamespace + "\", \"" +  rel + "\"," + symm + ");";
+    		 		lore.ore.ui.graph.ContextmenuConnection.contextMenu.appendMenuItem(new draw2d.MenuItem(rel,null,new Function(functionstr)));
+    		 	}
+    	        lore.ore.ui.graph.ContextmenuConnection.loadedOntology = om.ontologyURL;
+    			return lore.ore.ui.graph.ContextmenuConnection.contextMenu;
+    	    }
+        } catch (ex){ 
+            lore.debug.ore("problem generating context menu for connection",ex);
+            return null;
+        }
+    },
+    /** Override to make decorator look better on very short lines */
+    getEndAngle : function() {
+        var angle = 180;
+        try {
+    	  var p1 = this.lineSegments.get(this.lineSegments.getSize()-1).end;
+    	  var p2 = this.lineSegments.get(this.lineSegments.getSize()-1).start;
+    	  if(this.router instanceof draw2d.BezierConnectionRouter) {
+    	   p2 = this.lineSegments.get(this.lineSegments.getSize()-5).end;
+    	  }
+    	  var length = Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+    	  // adjustment for short lines
+    	  if (length < 8) {
+    	    p2 = this.lineSegments.get(this.lineSegments.getSize()-20).end;
+    	    length = Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+    	  }
+    	  angle = -(180/Math.PI) *Math.asin((p1.y-p2.y)/length);
+    	  
+    	  if (angle<0) {
+    	     if(p2.x<p1.x) {
+    	       angle = Math.abs(angle) + 180;
+             } else {
+    	       angle = 360- Math.abs(angle);
+             }
+    	  }
+    	  else {
+    	     if (p2.x < p1.x) {
+    	       angle = 180-angle;
+             }
+    	  }
+        } catch (e) {
+    	    lore.debug.ore("ContextMenuConnection.getEndAngle: error", e);
+    	}
+      return angle;
     }
-};
-lore.ore.ui.graph.ContextmenuConnection.prototype.createHTMLElement=function() {
-    var item = draw2d.Connection.prototype.createHTMLElement.call(this);
-    item.className = "ctxtConn";
-    return item;
-}
-/**
- * Construct the context menu for selecting the connection type
- * @return {}
- */
-lore.ore.ui.graph.ContextmenuConnection.prototype.getContextMenu=function() {
-    try {
-	    // use the cached menu if the relationship ontology has not changed
-	    if (lore.ore.ui.graph.ContextmenuConnection.contextMenu && (
-	            lore.ore.ui.graph.ContextmenuConnection.loadedOntology == lore.ore.onturl)) {
-	        return lore.ore.ui.graph.ContextmenuConnection.contextMenu;
-	    } else {
-	        lore.debug.ore("generating context menu for connection",this);
-		    lore.ore.ui.graph.ContextmenuConnection.contextMenu=new draw2d.Menu();
-			// sort the menu entries alphabetically
-            var om = lore.ore.ontologyManager;
-			var keys = [];
-		 	for (var rel in om.ontrelationships) {
-				keys.push(rel);
-		 	}
-		 	keys.sort();
-            
-		 	for (var i =0; i< keys.length; i++) {
-		 		rel = keys[i];
-				var relnamespace=om.ontrelationships[rel]; 
-	            var symmquery = om.ontology.prefix('rdf',lore.constants.NAMESPACES["rdf"])
-	                .where('<' + relnamespace + rel +'> rdf:type <' + lore.constants.OWL_SPROP + '>');
-		        var symm = symmquery.length > 0;
-				var functionstr = "lore.ore.ui.graphicalEditor.getSelectedFigure().setRelationship(\"" + relnamespace + "\", \"" +  rel + "\"," + symm + ");";
-		 		lore.ore.ui.graph.ContextmenuConnection.contextMenu.appendMenuItem(new draw2d.MenuItem(rel,null,new Function(functionstr)));
-		 	}
-	        lore.ore.ui.graph.ContextmenuConnection.loadedOntology = om.ontologyURL;
-			return lore.ore.ui.graph.ContextmenuConnection.contextMenu;
-	    }
-    } catch (ex){ 
-        lore.debug.ore("problem generating context menu for connection",ex);
-        return null;
-    }
-};
-/** Override to make decorator look better on very short lines */
-lore.ore.ui.graph.ContextmenuConnection.prototype.getEndAngle=function() {
-    var angle = 180;
-    try {
-	  var p1 = this.lineSegments.get(this.lineSegments.getSize()-1).end;
-	  var p2 = this.lineSegments.get(this.lineSegments.getSize()-1).start;
-	  if(this.router instanceof draw2d.BezierConnectionRouter) {
-	   p2 = this.lineSegments.get(this.lineSegments.getSize()-5).end;
-	  }
-	  var length = Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
-	  // adjustment for short lines
-	  if (length < 8) {
-	    p2 = this.lineSegments.get(this.lineSegments.getSize()-20).end;
-	    length = Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
-	  }
-	  angle = -(180/Math.PI) *Math.asin((p1.y-p2.y)/length);
-	  
-	  if (angle<0) {
-	     if(p2.x<p1.x) {
-	       angle = Math.abs(angle) + 180;
-         } else {
-	       angle = 360- Math.abs(angle);
-         }
-	  }
-	  else {
-	     if (p2.x < p1.x) {
-	       angle = 180-angle;
-         }
-	  }
-    } catch (e) {
-	    lore.debug.ore("ContextMenuConnection.getEndAngle: error", e);
-	}
-  return angle;
-}
+});
