9509e7d2a4a8403c957c4145f49f88f22bd4185b
Damien Ayers <damien@omad.net>
2010-12-02 01:58:48 +0000

    formatting/spacing

--- oaiorebuilder/content/uiglobal.js
+++ oaiorebuilder/content/uiglobal.js
@@ -1,40 +1,40 @@
 /*
  * Copyright (C) 2008 - 2009 School of Information Technology and Electrical
  * Engineering, University of Queensland (www.itee.uq.edu.au).
- * 
+ *
  * This file is part of LORE. LORE was developed as part of the Aus-e-Lit
  * project.
- * 
+ *
  * LORE is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * LORE is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * LORE. If not, see <http://www.gnu.org/licenses/>.
  */
 
-	var EXPORTED_SYMBOLS = ['ui'];	
+	var EXPORTED_SYMBOLS = ['ui'];
 	/**
      * @singleton
-     * @class lore.global.ui 
+     * @class lore.global.ui
 	 */
 	ui = {};
-	
+
 	Components.utils.import("resource://lore/debug.js");
 	Components.utils.import("resource://lore/util.js");
-	
+
 	/**
 	 * Display an informational message to the user
 	 * @param {String} message The message to display
 	 */
 	ui.loreInfo = function(message){
 	};
-	
+
 	/**
 	 * Display an warning message to the user
 	 * @param {String} message The message to display
@@ -57,7 +57,7 @@
         } catch (e1) {
             debug.ui ("error unregistering coview: " + e1,e1);
         }
-        try {    
+        try {
 			ui.annotationView.unregisterView();
         } catch (e2) {
             debug.ui("error unregistering annoview: " + e2,e2);
@@ -68,9 +68,9 @@
 			debug.ui ("error loading ui on reset: " + e3,e3);
 		}
 	};
-	
+
 	ui.resetAnnotations = function ( win, instId ) {
-		 try {    
+        try {
 			ui.annotationView.unregisterView();
         } catch (e2) {
             debug.ui("error unregistering annoview: " + e2,e2);
@@ -81,7 +81,7 @@
 			debug.ui ("error loading ui on reset: " + e3,e3);
 		}
 	};
-	
+
 	ui.resetCompoundObjects = function ( win, instId ) {
 		try {
 			ui.compoundObjectView.unregisterView();
@@ -94,36 +94,34 @@
 			debug.ui ("error loading ui on reset: " + e3,e3);
 		}
 	};
-	
+
 	ui.loadAnnotations  = function ( win, instId, reload ) {
 		var iframe1 = win.document.getElementById("annographiframe");
-		
+
 		iframe1.addEventListener("load", function onLoadTrigger(event){
-			iframe1.removeEventListener("load", onLoadTrigger, true);
-			iframe1.contentWindow.instanceId = instId;
-            iframe1.contentWindow.lore.anno.ui.init();
+                    iframe1.removeEventListener("load", onLoadTrigger, true);
+                    iframe1.contentWindow.instanceId = instId;
+                    iframe1.contentWindow.lore.anno.ui.init();
 		}, true);
 		if (reload) {
 			win.annographiframe.location.reload(true);
-		}
-		else {
+		} else {
 			iframe1.setAttribute("src", "chrome://lore/content/annotations/loreui_anno.html");
 		}
 	};
-	
+
 	ui.loadCompoundObjects = function ( win, instId, reload ) {
-		var iframe2 = win.document.getElementById("graphiframe");
-		iframe2.addEventListener("load", function onLoadTrigger(event){
-        	iframe2.removeEventListener("load", onLoadTrigger, true);
-            iframe2.contentWindow.instanceId = instId;
-            iframe2.contentWindow.lore.ore.ui.init();
-		}, true);
-		if (reload) {
-			win.graphiframe.location.reload(true);
-		}
-	    else {
-		    iframe2.setAttribute("src", "chrome://lore/content/compound_objects/loreui_ore.html");
-	    }
+            var iframe2 = win.document.getElementById("graphiframe");
+            iframe2.addEventListener("load", function onLoadTrigger(event){
+                iframe2.removeEventListener("load", onLoadTrigger, true);
+                iframe2.contentWindow.instanceId = instId;
+                iframe2.contentWindow.lore.ore.ui.init();
+            }, true);
+            if (reload) {
+                win.graphiframe.location.reload(true);
+            } else {
+                iframe2.setAttribute("src", "chrome://lore/content/compound_objects/loreui_ore.html");
+            }
 	};
 	/**
      * @param {} win
@@ -134,7 +132,7 @@
 		ui.loadAnnotations(win, instId, reload );
         ui.loadCompoundObjects(win, instId, reload);
 	};
-	
+
 	/**
 	 * Called just before the browser window is closed
 	 */
@@ -150,7 +148,7 @@
 			}
 		}
 	};
-	
+
 	/**
 	 * Called as the window is unloading, just before it disappears
 	 */
@@ -169,7 +167,7 @@
 			ui.currentURLs = {};
 		ui.currentURLs[instance] = url;
 	};
-	
+
 	ui.getCurrentURL = function (instance) {
 		return ui.currentURLs ? ui.currentURLs[instance]: null;
 	};
@@ -184,81 +182,76 @@
         }
         return ui.genInstanceID.counter;
     };
-	
-	/**
-	 * UI View 
-	 * The intention is that the views are never directly accesses via their iframes
-	 * by other view/overlay code
-     * @param {} args
-	 */
-		function Views(args){
 
-			this.name = args.name;
-			this.views = {};
-			this.events = {};
-		
-			this.loaded = function(instId){
-				return this.views[instId] != null;
-			};
-			
-			this.onload = function(instId, callback) {
-				
-				if ( !this.events[instId] )
-					this.events[instId] = [callback];
-				else
-					this.events[instId].push(callback);
-			};		
-			
-			this.registerView = function(view, instId){
-				this.views[instId] = view;
-				var e = this.events[instId];
-				if ( e && e.length > 0) {
-					for ( var i =0; i < e.length; i++) {
-						e[i](instId);
-					}
-				}
-				this.events[instId] = null;
-			};
-			
-			this.unregisterView = function(instId){
-				if (!instId) {
-					for (x in this.views) {
-						if (this.views[x].uninit) {
-							this.views[x].uninit();
-						}
-					}
-					this.views = {};
-				}
-				else {
-					try {
-						if (this.views[instId].uninit) {
-								this.views[instId].uninit();
-						}
-					} catch (e) {
-						debug.ui('unregisterView(' + instId + ')', e);
-					}
-					delete this.views[instId];
-					delete this.events[instId];		
-				}
-			};
-			
-			this.get = function(instId){
-				return this.views[instId];
-			};
-		}
-		
-		
-		ui.compoundObjectView = new Views({
-			name: 'coView'
-		});
-		ui.annotationView = new Views({
-			name: 'annoView'
-		});
-		ui.topWindowView = new Views({
-			name: 'topView'
-		});
-        
-	
-	
-	
-	
+/**
+* UI View
+* The intention is that the views are never directly accesses via their iframes
+* by other view/overlay code
+* @param {} args
+*/
+function Views(args){
+
+    this.name = args.name;
+    this.views = {};
+    this.events = {};
+
+    this.loaded = function(instId){
+            return this.views[instId] != null;
+    };
+
+    this.onload = function(instId, callback) {
+
+            if ( !this.events[instId] )
+                    this.events[instId] = [callback];
+            else
+                    this.events[instId].push(callback);
+    };
+
+    this.registerView = function(view, instId){
+            this.views[instId] = view;
+            var e = this.events[instId];
+            if ( e && e.length > 0) {
+                    for ( var i =0; i < e.length; i++) {
+                            e[i](instId);
+                    }
+            }
+            this.events[instId] = null;
+    };
+
+    this.unregisterView = function(instId){
+            if (!instId) {
+                    for (x in this.views) {
+                            if (this.views[x].uninit) {
+                                    this.views[x].uninit();
+                            }
+                    }
+                    this.views = {};
+            }
+            else {
+                    try {
+                            if (this.views[instId].uninit) {
+                                            this.views[instId].uninit();
+                            }
+                    } catch (e) {
+                            debug.ui('unregisterView(' + instId + ')', e);
+                    }
+                    delete this.views[instId];
+                    delete this.events[instId];
+            }
+    };
+
+    this.get = function(instId){
+            return this.views[instId];
+    };
+}
+
+
+ui.compoundObjectView = new Views({
+        name: 'coView'
+});
+ui.annotationView = new Views({
+        name: 'annoView'
+});
+ui.topWindowView = new Views({
+        name: 'topView'
+});
