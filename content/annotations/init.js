37d032e2488d85a6cdef57d4c01c0c04a8e81d41
Damien Ayers <damien@omad.net>
2011-02-07 04:04:34 +0000

    Fixes for LORE getting confused when tabs change but the URL doesn't. #212
    Also refactoring to split the annotations sidebar code out into it's own file, and start removing unneeded globals.

--- content/annotations/init.js
+++ content/annotations/init.js
@@ -53,16 +53,7 @@ lore.anno.ui.extension = Components.classes["@mozilla.org/extensions/manager;1"]
  */
 
 // for reference
-/**
- * Ext GUI Configuration
- * @property gui_spec
- */
-lore.anno.ui.gui_spec = null;
-/**
- * Ext main window
- * @property main_window
- */
-lore.anno.ui.main_window = null;
+
 /**
  * Reference to the parent view of the annotation view
  * @property topView
@@ -113,7 +104,7 @@ lore.anno.ui.init = (function() {
        try {
            // obtain reference to parent overlay reference
            lore.anno.ui.topView = lore.global.ui.topWindowView.get(window.instanceId);
-   
+
            // set up preferences
            lore.anno.prefs = new lore.anno.Preferences({
                creator: 'Anonymous',
@@ -129,7 +120,6 @@ lore.anno.ui.init = (function() {
    
            lore.anno.prefs.on('prefs_changed', lore.anno.ui.handlePrefsChange);
    
-           lore.anno.ui.currentContentWindow = lore.global.util.getContentWindow(window);
            lore.anno.ui.currentURL = lore.global.util.getContentWindow(window).location.href;
            lore.anno.annoMan = new lore.anno.AnnotationManager({
                url: lore.anno.ui.currentURL,
@@ -179,242 +169,14 @@ lore.anno.ui.init = (function() {
             rdfaManager: rdfaMan,
             model: annotationManager.annods
         });
-       initGUIConfig(rdfaMan);
+        
+        new lore.anno.ui.Sidebar({
+                rdfaManager       : rdfaMan,
+                annotationManager : annotationManager,
+                pageView          : lore.anno.ui.pageui});
     }
     
-    /*
-     * Construct the Ext based GUI and initialize and show the components
-     */
-    function initGUIConfig(rdfaManager) {
-    
-        /*
-         * the "Using Annotations" tab
-         */
-        var aboutPanel = {
-            title: "Using Annotations",
-            id: "about",
-            iconCls: "welcome-icon",
-            xtype: 'container',
-            autoEl: {
-                tag: 'iframe',
-                style: 'border:none',
-                src: 'chrome://lore/content/annotations/about_annotations.html',
-                width: '100%',
-                height: '100%'
-            }
-        }
-    
-        /*
-         * 'Annotation Editor' panel
-         */
-        var loreuieditor = {
-            xtype: 'annoeditorpanel',
-            region: "south",
-            split: true,
-            height: 300,
-            pageView: lore.anno.ui.pageui,
-            rdfaManager: rdfaManager,
-            model: lore.anno.annoMan.annodsunsaved,
-            id: "annotationslistform",
-            annomode: lore.constants.ANNOMODE_NORMAL,
-            buttonsConfig: [{
-                text: 'Cancel',
-                id: 'canceleditbtn',
-                tooltip: 'Cancel Editing this Annotation - Discards any changes'
-            },
-            {
-                text: 'Hide Editor',
-                id: 'hideeditbtn',
-                tooltip: 'Hides the annotation editor from view'
-            },
-            {
-                text: 'Save',
-                id: 'updannobtn',
-                tooltip: 'Save the annotation to the repository'
-            }]
-        }
-    
-        /*
-         * the panel that contains the annotation tree view and editor
-         */
-        var browsePanel = {
-            title: "Browse",
-            xtype: "container",
-            id: "treeview",
-            layout: "border",
-            items: [{
-                region: "center",
-                xtype: "annocolumntreepanel",
-                id: "annosourcestree",
-                model: lore.anno.annoMan.annods,
-                animate: false
-            },
-            loreuieditor]
-        }
-    
-        /*
-         * Helper function that constructs the 'Search' tab
-         */
-        var searchPanel = {
-            xtype: 'annosearchpanel',
-            layout: 'border',
-            id: 'searchpanel',
-            model: lore.anno.annoMan.annosearchds,
-            annoManager: lore.anno.annoMan
-        }
-    
-    
-        try {
-            lore.anno.ui.tabpanel = new Ext.TabPanel({
-                anchor: "100% -25",
-                xtype: "tabpanel",
-                title: "Navigation",
-                id: "navigationtabs",
-                deferredRender: false,
-                activeTab: "treeview",
-                items: [browsePanel, searchPanel, aboutPanel]
-            });
-    
-            lore.anno.ui.gui_spec = {
-                layout: "anchor",
-                border: false,
-                items: [lore.anno.ui.tabpanel,
-                {
-                    anchor: "100%",
-                    height: 25,
-                    xtype: "statusbar",
-                    id: "status",
-                    defaultText: "",
-                    autoClear: 6000
-                }]
-            };
-    
-            lore.anno.ui.main_window = new Ext.Viewport(lore.anno.ui.gui_spec);
-            lore.anno.ui.main_window.show();
-    
-            initExtComponents();
-        }
-        catch (ex) {
-            lore.debug.ui("Exception creating anno UI", ex);
-        }
-    }
-    /*
-     * Initialize the Ext Components. Sets globals, visibility of fields
-     * and initialize handlers
-     */
-    function initExtComponents() {
-        try {
-    
-            initUIGlobals();
-            attachContextMenus();
-            attachHandlers();
-    
-            lore.anno.ui.formpanel.setAnnotationFormUI(false, false);
-    
-            // tooltips
-            Ext.QuickTips.interceptTitles = true;
-            Ext.QuickTips.init();
-            Ext.apply(Ext.QuickTips.getQuickTip(), {
-                dismissDelay: 0
-            });
-    
-            lore.anno.ui.formpanel.setPreferences(lore.anno.prefs);
-    
-        } catch (e) {
-            lore.debug.ui("Errors during initExtComponents: " + e, e);
-        }
-    }
-    /*
-     * Initialize the globals for the UI controls
-     */
-    function initUIGlobals() {
-    
-        lore.anno.ui.views = Ext.getCmp("treeview");
-    
-        // set up the root tree node, and the two main child nodes 'Current Page' and 'Unsaved Changes'
-        lore.anno.ui.treerealroot = Ext.getCmp("annosourcestree").getRootNode();
-        lore.anno.ui.treeroot = new lore.anno.ui.AnnoPageTreeNode({
-            text: 'Current Page',
-            model: lore.anno.annoMan.annods
-        });
-        lore.anno.ui.treeunsaved = new lore.anno.ui.AnnoModifiedPageTreeNode({
-            text: 'Unsaved Changes',
-            model: lore.anno.annoMan.annodsunsaved,
-            postfix: "-unsaved"
-        });
-    
-        lore.anno.ui.treerealroot.appendChild([lore.anno.ui.treeroot, lore.anno.ui.treeunsaved]);
-        lore.anno.ui.treeroot.expand();
-    
-        // Auto expand child nodes
-        Ext.getCmp("annosourcestree").on("expandnode", function (node) {
-            if (node != lore.anno.ui.treeroot) {
-                node.expandChildNodes(false);
-            }
-        });
-    
-        lore.anno.ui.formpanel = Ext.getCmp("annotationslistform");
-        lore.anno.ui.formpanel.hide();
-    
-        lore.anno.ui.timeline = Ext.getCmp("annotimeline");
-        lore.anno.ui.search = Ext.getCmp("searchpanel");
-    }
-    
-    /*
-     * Add context menu ( right-click) to controls
-     */
-    function attachContextMenus() {
-    
-        // Context Menu for 'Annotation List' tab
-        lore.anno.ui.views.contextmenu = new Ext.menu.Menu({
-            id: "anno-context-menu"
-        });
-        lore.anno.ui.views.contextmenu.add({
-            text: "Show RDF/XML",
-            handler: function () {
-                try {
-                    lore.anno.ui.openView("remrdfview", "RDF/XML", function () {
-                        Ext.getCmp("remrdfview").body.update(lore.global.util.escapeHTML(lore.anno.serialize("rdf")));
-                    });
-                } catch (e) {
-                    lore.debug.anno("Error generating RDF view: " + e, e);
-                }
-            }
-        });
-    
-        lore.anno.ui.views.on("contextmenu", function (tabpanel, tab, e) {
-            lore.anno.ui.views.contextmenu.showAt(e.xy);
-        });
-    
-        // Add handler to add Context Menu for Tree Nodes when they're appended
-        lore.anno.ui.treeroot.on('append', lore.anno.ui.handleAttachAnnoCtxMenuEvents);
-    }
-    
-    /*
-     * Attach event handlers to UI events
-     */
-    function attachHandlers() {
-        // Add default behaviour when a new annotation is added
-        lore.anno.ui.treeroot.on('append', lore.anno.ui.handleAttachNodeLinks);
-    
-        // Tree node is clicked/double clicked
-        Ext.getCmp("annosourcestree").on("click", lore.anno.ui.handleTreeNodeSelection);
-        Ext.getCmp("annosourcestree").on("dblclick", lore.anno.ui.handleEditTreeNode);
-    
-        // editor handlers
-        Ext.getCmp("hideeditbtn").on('click', lore.anno.ui.handleHideAnnotationEditor);
-        Ext.getCmp("updannobtn").on('click', lore.anno.ui.handleSaveAnnotationChanges);
-        Ext.getCmp("canceleditbtn").on('click', lore.anno.ui.handleCancelEditing);
-    
-        // Annotation Manager
-        lore.anno.annoMan.on('annotationsloaded', lore.anno.ui.handleAnnotationsLoaded);
-        lore.anno.annoMan.on('committedannotation', lore.anno.ui.handleCommittedAnnotation);
-        lore.anno.annoMan.on('servererror', lore.anno.ui.handleServerError);
-    
-        // Authentication Manager
-        lore.anno.am.on('signedin', lore.anno.ui.topView.setAnnotationsSignedIn);
-        lore.anno.am.on('signedout', lore.anno.ui.topView.setAnnotationsSignedOut);
-    }
+
    return publicInit;
 })();
 
