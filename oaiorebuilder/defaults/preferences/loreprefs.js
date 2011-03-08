9a734b442fda9aa94a5ce4ec92049e7c6be8d851
Anna Gerber <anna.m.gerber@gmail.com>
2011-03-08 04:07:14 +0000

    New preference to associate ontologies, prefixes and namespaces

--- oaiorebuilder/defaults/preferences/loreprefs.js
+++ oaiorebuilder/defaults/preferences/loreprefs.js
@@ -18,3 +18,4 @@ pref("extensions.lore.high_contrast", false);
 pref("extensions.lore.annocache_timeout", 1); // effectively disable for the moment
 pref("extensions.lore.annotationMetadataOntologyURL", "chrome://lore/content/ontologies/AustLit.xml");
 pref("extensions.lore.createprivateannotations", false);
+pref("extensions.lore.ontologies",'[{\"nsprefix\":\"austlit\",\"locurl\":\"chrome://lore/content/ontologies/AustLit.xml\",\"useanno\":\"true\",\"useco\":\"false\", \"status\":\"default\", \"nsuri\":\"http://austlit.edu.au/owl/austlit.owl#\"},{\"nsuri\":\"http://RDVocab.info/Elements/\",\"nsprefix\":\"rda\",\"locurl\":\"chrome://lore/content/ontologies/rda.rdf\",\"useanno\":\"false\",\"useco\":\"false\", \"status\":\"custom\"},{\"nsprefix\":\"lore\", \"locurl\":\"chrome://lore/content/ontologies/austlitoaiore.owl\",\"useanno\":\"false\",\"useco\":\"true\", \"status\":\"default\", \"nsuri\":\"http://austlit.edu.au/owl/austlitore.owl#\"}]');
