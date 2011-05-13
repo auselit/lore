2b62aed867155553279a3b24ae22bbfb3c892100
Anna Gerber <anna.m.gerber@gmail.com>
2011-05-13 01:11:10 +0000

    New version of AuthManager (to be shared between Compound Objects and Annotations)

---
+++ oaiorebuilder/content/AuthManager.js
@@ -0,0 +1,240 @@
+/*
+ * Copyright (C) 2008 - 2010 School of Information Technology and Electrical
+ * Engineering, University of Queensland (www.itee.uq.edu.au).
+ *
+ * This file is part of LORE. LORE was developed as part of the Aus-e-Lit
+ * project.
+ *
+ * LORE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * LORE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * LORE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+Ext.ns('lore');
+
+lore.AuthManager = Ext.extend(Ext.util.Observable, {
+    // TODO: check authorities independently: for now we do not support accounts without both roles
+    ANNO_AUTHORITY: "ROLE_ANNOTATOR",
+    ORE_AUTHORITY: "ROLE_ORE",
+    constructor: function() {
+        this.addEvents(
+			/**
+			 * @event signedin
+			 * Fires when the user has successfully signed into the server
+			 */
+			'signedin',
+            /**
+			 * @event signedin
+			 * Fires when the user has signed out from the server.
+			 */
+			'signedout',
+            /**
+             * @event cancel
+             * Fires when user cancels logging in
+             */
+            'cancel',
+            /**
+             * @event error
+             * Fires when authorisation cannot be established due to error
+             */
+            'error');
+        // Tracks signed in state, to prevent superfluous event firing
+        this.signedIn = false; 
+    },
+    
+    reloadEmmetUrls: function(prefs) {
+        var emmetUrl = prefs.url + '/emmet.svc';
+        lore.debug.ui("reloadEmmetUrls " + emmetUrl);
+        if (this.EMMET_URL == emmetUrl) {
+            return;
+        } else {
+            this.EMMET_URL = emmetUrl;
+        }
+
+        Ext.Ajax.request({
+            url: this.EMMET_URL,
+            success: this.parseEmmetUrlsResponse,
+            method: 'GET',
+            params: {action: 'fetchEmmetUrls',
+                     format: 'json'},
+            scope: this
+
+        });
+    },
+
+    parseEmmetUrlsResponse: function(response) {
+        var jsObject = Ext.decode(response.responseText);
+        var emmetUrls = jsObject.emmetUrls;
+        this.LOGOUT_URL = emmetUrls['emmet.logout.url'];
+        this.LOGIN_URL = emmetUrls['emmet.login.url'];
+        this.REGISTER_URL = emmetUrls['emmet.register.url'];
+
+        this.isAuthenticated();
+    },
+
+
+    /**
+     * Run the supplied callback function if the user is currently authorised
+     */
+    isAuthenticated : function(callback) {
+        lore.debug.ui("isAuthenticated",this);
+        if (this.EMMET_URL){
+	        Ext.Ajax.request({
+	           url: this.EMMET_URL,
+	           success: this.checkAuthentication,
+	           failure: this.fireError.createDelegate(this),
+	           method: 'GET',
+	           params: { action: 'fetchAuthentication',
+	                     format: 'json' },
+	           callIfAuthorised: callback,
+	           scope: this
+	        });
+        } else {
+            lore.debug.ui("No emmet url defined!",this);
+        }
+    },
+
+    /**
+     * Run the supplied callback function if the user is *not* currently authorised
+     */
+    ifNotAuthenticated : function(callback) {
+        lore.debug.ui("ifNotAuthenticated",this);
+        if (this.EMMET_URL){
+	        Ext.Ajax.request({
+	           url: this.EMMET_URL,
+	           success: this.checkAuthentication,
+	           failure: this.fireError.createDelegate(this),
+	           method: 'GET',
+	           params: { action: 'fetchAuthentication',
+	                     format: 'json' },
+	           callIfNotAuthorised: callback,
+	           scope: this
+	        });
+        }
+    },
+
+    // private
+    checkAuthentication : function(xhr, options) {
+    	try {
+            var principal = Ext.decode(xhr.responseText).userAuthentication.principal;
+            lore.debug.ui("checkAuthentication: ", principal);
+            var authorities = principal.authorities;
+            var authorised = this.hasAuthority(authorities, this.ANNO_AUTHORITY) &&
+                this.hasAuthority(authorities, this.ORE_AUTHORITY);
+
+            if (authorised) {
+            	this.fireSignedIn(principal.userName);
+                if (typeof options.callIfAuthorised == 'function') {
+                    options.callIfAuthorised(principal);
+                }
+                return;
+            }
+    	} catch (e) {
+    		lore.debug.anno("AuthManager:checkAuthentication failed", e);
+    	}
+        lore.debug.ui("User is not authorised");
+        this.fireSignedOut();
+        if (typeof options.callIfNotAuthorised == 'function') {
+            options.callIfNotAuthorised();
+        }
+    },
+
+    // private
+    hasAuthority : function(authorities, requiredAuthority) {
+        if (authorities) {
+            for (var i = 0; i < authorities.length; i++) {
+                if (authorities[i].authority == requiredAuthority) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+
+    /**
+     * Checks whether the user is currently authorised,
+     * if so, runs the callback function, if not
+     * displays a loginwindow, and if successful runs the function.
+     * If not, the function is not run.
+     */
+    runWithAuthorisation : function(callback) {
+        var t = this;
+        this.isAuthenticated(function(principal) {
+            if (typeof callback == 'function') {
+                callback(principal);
+            }
+        });
+        this.ifNotAuthenticated(function() {
+            t.popupLoginWindow(callback);
+        });
+    },
+
+    /**
+     * If the user is not already authorised, open a login dialog for them to do so
+     */
+    displayLoginWindow : function() {
+        var t = this;
+        this.ifNotAuthenticated(function () {
+            t.popupLoginWindow();
+        });
+    },
+
+    //private
+    popupLoginWindow : function(callback) {
+        try{
+        var winOpts = 'height=250,width=470,top=200,left=250,resizable,scrollbars=yes,dependent=yes';
+        var loginwindow = window.openDialog("chrome://lore/content/loginWindow.xul", 'lore_login_window', winOpts,
+                                            {initURL: this.LOGIN_URL,
+                                             logger: lore.debug.ui});
+
+        var t = this;
+        loginwindow.addEventListener("close", function() {
+                t.fireEvent("cancel");
+                t.isAuthenticated(callback);
+        }, false);
+        loginwindow.addEventListener('DOMWindowClose', function() {
+                t.fireEvent("cancel");
+                t.isAuthenticated(callback);
+        }, false);
+        } catch (ex){
+            lore.debug.ui("popupLoginWindow",ex);
+        }
+    },
+    
+    logout : function() {
+        Ext.Ajax.request({
+            url: this.LOGOUT_URL,
+            success: this.isAuthenticated,
+            method: 'GET',
+            scope: this
+         });
+    },
+    
+    fireSignedIn : function(userName) {
+    	if (!this.signedIn) {
+    		this.signedIn = true;
+    		this.fireEvent('signedin', userName);
+    	}
+    },
+    
+    fireSignedOut : function() {
+    	if (this.signedIn) {
+    		this.signedIn = false;
+            this.fireEvent('signedout');
+    	}  
+    },
+    fireError : function(){
+        this.fireSignedOut();
+        this.fireEvent('error');
+    }
+
+});
