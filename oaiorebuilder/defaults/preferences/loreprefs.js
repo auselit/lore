3ed3c4b7b625a8233d10d262ed7d3ef12cfb1a7a
Anna Gerber <anna.m.gerber@gmail.com>
2011-05-16 02:29:00 +0000

    reorganised prefs

--- oaiorebuilder/defaults/preferences/loreprefs.js
+++ oaiorebuilder/defaults/preferences/loreprefs.js
@@ -1,9 +1,10 @@
 pref("extensions.lore.relonturl", "chrome://lore/content/ontologies/austlitoaiore.owl");
 pref("extensions.lore.dccreator","Anonymous");
-pref("extensions.lore.rdfrepos", "http://austlit.edu.au/openrdf-sesame/repositories/lore");
-pref("extensions.lore.rdfrepostype", "sesame");
+pref("extensions.lore.rdfrepos", "http://auselit.metadata.net/danno/ore/");
+pref("extensions.lore.rdfrepostype", "lorestore");
+pref("extensions.lore.coeditor","grapheditor");
 pref("extensions.lore.tmkey","");
-pref("extensions.lore.annoserver", "http://auselit.metadata.net/danno/annotea");
+pref("extensions.lore.annoserver", "http://auselit.metadata.net/danno");
 pref("extensions.lore.annotationmode", false);
 pref("extensions.lore.DBG_LORE_UI", false);
 pref("extensions.lore.DBG_LORE_ANNOTATIONS", false);
