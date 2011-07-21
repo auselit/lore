d6021d91d0dc5d2aa6dc1e4ca80471f59255de44
Anna Gerber <anna.m.gerber@gmail.com>
2011-07-21 01:26:42 +0000

    Display last modified data in annotation data view

--- content/annotations/ui/AnnotationsDataView.js
+++ content/annotations/ui/AnnotationsDataView.js
@@ -7,9 +7,8 @@ lore.anno.ui.AnnotationsDataView = Ext.extend(Ext.DataView, {
                     '<div class="annoListingTitle">{title}</div>',
                     '<div class="annoURL">{resource}</div>',
                     '<div class="annoListingMeta">{creator}',
-//                        ', created {[lore.global.util.shortDate(values.created,Date)]}',
                         ', created {created:date("M j, Y")}',
-//                        '<tpl if="typeof modified != \'undefined\' && modified != null && modified != \'\'">, last modified {[lore.global.util.shortDate(values.modified,Date)]}</tpl>',
+                        '<tpl if="typeof modified != \'undefined\' && modified != null && modified != \'\'">, last modified {modified:date("M j, Y")}</tpl>',
                     '</div>',
                 '</div>',
                 '</tpl>'
@@ -19,7 +18,7 @@ lore.anno.ui.AnnotationsDataView = Ext.extend(Ext.DataView, {
             autoScroll: true,
             itemSelector: "div.annoListing",
             overClass:'x-view-over',
-            emptyText: 'No annotations to display'
+            emptyText: 'No annotations / annotations loading'
         });
         lore.anno.ui.AnnotationsDataView.superclass.initComponent.apply(this,arguments);
         
@@ -63,17 +62,3 @@ lore.anno.ui.AnnotationsDataView = Ext.extend(Ext.DataView, {
     }
 });
 Ext.reg('annodataview', lore.anno.ui.AnnotationsDataView);
-
-
-// lore.anno.ui.getAnnoTypeIcon(rec.data);
-// modified
-
-//Ext.override(Ext.DataView, {
-//    setTemplate: function(tpl) {
-//        this.tpl = tpl;
-//        if (typeof this.tpl == 'string')
-//	        this.tpl = new Ext.XTemplate(this.tpl);
-//        }
-//        this.refresh();
-//    }
-//);
