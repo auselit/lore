8519f2bf14fd0949132c6d95f7882f3d0673b301
Anna Gerber <anna.m.gerber@gmail.com>
2012-02-27 13:25:23 +1000

    Allow resources to not have a mimetype

--- content/compound_objects/ui/graph/ResourceFigure.js
+++ content/compound_objects/ui/graph/ResourceFigure.js
@@ -476,8 +476,10 @@ Ext.extend(lore.ore.ui.graph.ResourceFigure, lore.ore.ui.graph.EntityFigure, {
                         if (xhr.status == 200){
                             try {
                                 mimetype = xhr.getResponseHeader('Content-Type');
-                                // strip out any invalid characters from mimetype
-                                mimetype = mimetype.replace(/([\x09\x0A\x0D\x20-\x7E]|[\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}|\xED[\x80-\x9F][\x80-\xBF]|\xF0[\x90-\xBF][\x80-\xBF]{2}|[\xF1-\xF3][\x80-\xBF]{3}|\xF4[\x80-\x8F][\x80-\xBF]{2})|./g, "$1");
+                                if (mimetype){
+                                    // strip out any invalid characters from mimetype
+                                    mimetype = mimetype.replace(/([\x09\x0A\x0D\x20-\x7E]|[\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}|\xED[\x80-\x9F][\x80-\xBF]|\xF0[\x90-\xBF][\x80-\xBF]{2}|[\xF1-\xF3][\x80-\xBF]{3}|\xF4[\x80-\x8F][\x80-\xBF]{2})|./g, "$1");
+                                }
                             } catch (e) {
                                 lore.debug.ore("ResourceFigure: exception getting mime type", e);
                             }
