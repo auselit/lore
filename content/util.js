da8174627acc5b43087db038a076bb077b19f10f
Anna Gerber <anna.m.gerber@gmail.com>
2012-02-28 11:02:53 +1000

    Clearer comment re copyright of math.uuid

--- content/util.js
+++ content/util.js
@@ -1258,7 +1258,7 @@ lore.util = {
     },
 /*
 From Math.uuid.js 1.3
-Copyright (c) 2008, Robert Kieffer
+Math.uuid.js is Copyright (c) 2008, Robert Kieffer
 All rights reserved.
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
